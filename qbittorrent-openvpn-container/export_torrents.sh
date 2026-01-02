accd366935c8ac65cee13e76fee12ee2f295a784
MatteV02 <veronimatteo1@gmail.com>
2026-01-02 20:14:32 +0100

    add a safeguard to export_torrents: only save the exported torrents list if the torrents are get correctly from the api; create a backup of the active torrents list

--- qbittorrent-openvpn-container/export_torrents.sh
+++ qbittorrent-openvpn-container/export_torrents.sh
@@ -3,4 +3,9 @@
 # Fetch active torrents and their magnet links using qBittorrent Web API
 curl -s -X GET "http://localhost:8080/api/v2/torrents/info" -d "filter=downloading" -d "category=" \
   --header "Referer: http://localhost:8080" \
-  | jq -r '.[] | .magnet_uri' > /root/torrents/active_torrents.list
+  | jq -r '.[] | .magnet_uri' > /tmp/active_torrents.list
+if [ $? -eq 0 ] then
+  rm /root/torrents/active_torrents.list.bak
+  mv /root/torrents/active_torrents.list /root/torrents/active_torrents.list.bak
+  mv /tmp/active_torrents.list /root/torrents/active_torrents.list
+fi
